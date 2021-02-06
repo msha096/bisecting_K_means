ead3f7bdaf9df22785de924f251211d1fa91d186
Mingzhi <34358773+msha096@users.noreply.github.com>
2021-02-05 23:36:59 -0500

    update

--- model.py
+++ model.py
@@ -10,28 +10,38 @@ from sklearn.metrics import silhouette_samples, silhouette_score
 import heapq
 import os
 
-
-
 def compute_distance(c, cluster, features):
-    # cluster: index id to the feature table
+    '''
+    compute the square distance between each sample and the centroid.
+    cluster: index id to the feature table
+    features: the global features with index
+    '''
+    
     s = 0 
     for i in cluster:
         s += sum(features.loc[i] - c)**2
-    return s/len(cluster)
+    return s
 
 
 def calculate_s_score(value, label):
+    '''
+    Generate the silhouette score, which does not require ground truth labels.
+    '''
     silhouette_df = pd.DataFrame(list(zip(label, value)), columns=['cluster_id','score'])
     res = silhouette_df.groupby('cluster_id').mean()
     print('Silhouette Scores are %f and %f'%(res.loc[0],res.loc[1]))
 
 
 def k_means(cluster, n, feature_table, selection):
+    '''
+    Baseline K-Means where k is fixed to 2.
+    '''
     pca = PCA(n_components = n)
-    # features is the global feature with global index
+    # feature_table is the global feature with global index
     if selection:
+        # features: pca on global features
         features = pd.DataFrame(pca.fit_transform(feature_table))
-        X = features.loc[cluster]
+        X = features.loc[cluster] # X is the local features
     else:
         X = feature_table.loc[cluster]
     
@@ -52,20 +62,37 @@ def k_means(cluster, n, feature_table, selection):
             cluster_id2.append(cluster[i])
         
     # comput the squre distance
-    d1 = compute_distance(c[0],cluster_id1, features)
-    d2 = compute_distance(c[1],cluster_id2, features)
+    if selection:
+        d1 = compute_distance(c[0],cluster_id1, features)
+        d2 = compute_distance(c[1],cluster_id2, features)
+    else:
+        d1 = compute_distance(c[0],cluster_id1, feature_table)
+        d2 = compute_distance(c[1],cluster_id2, feature_table)
     return (-d1,cluster_id1), (-d2,cluster_id2)
 
 
+
 def linear_n(n, feature_table):
-    # n is positive-correlated with cluster size
+    ''' 
+    The algorithm to reduce the feature dimension.
+    n is positive-correlated with cluster size
+    '''
     cluster_ratio = n/len(feature_table)
     print('cluster_raio',cluster_ratio)
-    n = max(3,int(1000 * cluster_ratio))
+    n = max(3,int(150 * cluster_ratio))
     print('PCA to '+ str(n))
     return n
 
 def bisecting_kmeans(k, cluster, priority_queue, feature_table, selection):
+    '''
+    bisecting k-means model
+    k: the target total number of clusters at the end
+    cluster: a list of sample IDs/Indexes
+    priority_queue: a heapq to find the max square distance
+    feature_table: the global features
+    selection: boolean, weather to use the feature selection or not
+
+    '''
     while k > 1:
         if not priority_queue:
             print('Starting:\n')
@@ -84,9 +111,14 @@ def bisecting_kmeans(k, cluster, priority_queue, feature_table, selection):
 
 
 def tunning_k(max_k, feature, selection):
+    '''
+    The takeaway method to find the optimal k.
+    Looping from 2 to max_k.
+    '''
     queue = []
     p = [i for i in range(len(feature))]
-    for i in range(2,max_k):
+    for i in range(2, max_k):
+        print('\n')
         print('Processing with k = %d\n'%i)
         res = bisecting_kmeans(i, p, queue, feature, selection)
 
@@ -94,4 +126,4 @@ def tunning_k(max_k, feature, selection):
 if __name__ == '__main__':
     current_path = os.path.dirname(__file__)
     tf_idf = pd.read_csv(current_path + '/data/tf_idf_1k.csv')
-    tunning_k(max_k = 15, feature = tf_idf, selection = True)
+    tunning_k(max_k = 12, feature = tf_idf, selection = True)
